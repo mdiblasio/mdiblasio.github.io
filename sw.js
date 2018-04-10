14dbc4e920d258b569cee953934876249b765761
Michael DiBlasio <mdiblasio@mdiblasio-macbookpro01.roam.corp.google.com>
2018-04-10 17:35:55 -0400

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,36 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+importScripts('idb.js');
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+var dbPromise = idb.open('DynamicManifest', 1, (function(upgradeDb) {
+    if (!upgradeDb.objectStoreNames.contains('UserSettings')) {
+        var queuedRequests = upgradeDb.createObjectStore('UserSettings', {
+            keyPath: 'setting',
+        });
+    }
+}));
+
+function getStartURL() {
+    return dbPromise.then(function(db) {
+        var tx = db.transaction('UserSettings', 'readonly');
+        var store = tx.objectStore('UserSettings');
+        return store.get('start_url');
+    }).then(val => {
+        if (!val)
+            return 'home.html';
+        return val.value;
+    });
+}
+
+const handler = ({ url, event }) => {
+    return getStartURL().then(page => {
+    	return fetch(page)
+    });
+};
+
+workbox.routing.registerRoute(
+    /start/,
+    handler
+);
