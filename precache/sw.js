49a14e9626035dfe7a8a25b3f5f3f5892e0f0a87
Michael DiBlasio <mdiblasio@google.com>
2018-11-19 13:30:48 -0500

    Initial commit

--- precache/sw.js
+++ precache/sw.js
@@ -12,16 +12,16 @@ if (workbox) {
 }
 
 workbox.precaching.precacheAndRoute([
-    '/assets/1.js',
-    '/assets/2.js',
-    '/assets/3.js',
-    '/assets/4.js',
-    '/assets/5.js',
-    '/assets/1.css',
-    '/assets/2.css',
-    '/assets/3.css',
-    '/assets/4.css',
-    '/assets/5.css',
+    'assets/1.js',
+    'assets/2.js',
+    'assets/3.js',
+    'assets/4.js',
+    'assets/5.js',
+    'assets/1.css',
+    'assets/2.css',
+    'assets/3.css',
+    'assets/4.css',
+    'assets/5.css',
 ]);
 
 
