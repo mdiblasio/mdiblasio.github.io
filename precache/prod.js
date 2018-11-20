1bd04df18ffb285006bd1fd7b80e886933ab15f4
Michael DiBlasio <mdiblasio@google.com>
2018-11-19 23:20:07 -0500

    Initial commit

---
+++ precache/prod.js
@@ -0,0 +1,17 @@
+workbox.precaching.precacheAndRoute([
+  // {
+  //   url: 'index.html',
+  //   revision: '3'
+  // },
+  'assets/offline.html',
+  'assets/1.js',
+  'assets/2.js',
+  'assets/3.js',
+  'assets/4.js',
+  'assets/5.js',
+  'assets/1.css',
+  'assets/2.css',
+  'assets/3.css',
+  'assets/4.css',
+  'assets/5.css',
+]);
--- precache/sw.js
+++ precache/sw.js
@@ -1,7 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
+
 workbox.skipWaiting();
-workbox.clientsClaim();
+// workbox.clientsClaim();
 
 workbox.setConfig({ debug: true });
 
@@ -11,23 +12,25 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-workbox.precaching.precacheAndRoute([
-  // {
-  //   url: 'index.html',
-  //   revision: '3'
-  // },
-  'assets/offline.html',
-  'assets/1.js',
-  'assets/2.js',
-  'assets/3.js',
-  'assets/4.js',
-  'assets/5.js',
-  'assets/1.css',
-  'assets/2.css',
-  'assets/3.css',
-  'assets/4.css',
-  'assets/5.css',
-]);
+importScripts('prod.js');
+
+// workbox.precaching.precacheAndRoute([
+//   // {
+//   //   url: 'index.html',
+//   //   revision: '3'
+//   // },
+//   'assets/offline.html',
+//   'assets/1.js',
+//   'assets/2.js',
+//   'assets/3.js',
+//   'assets/4.js',
+//   'assets/5.js',
+//   'assets/1.css',
+//   'assets/2.css',
+//   'assets/3.css',
+//   'assets/4.css',
+//   'assets/5.css',
+// ]);
 
 const htmlHandler = workbox.strategies.networkOnly();
 // A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
